12fe5938e6dbb86e74a73423c844fc7bd0e13457
Sharon2408 <sharonantoy2408@gmail.com>
2023-06-13 13:00:20 +0530

    angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+// Pre defined component in Angular
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-project-app';
+  
+}
