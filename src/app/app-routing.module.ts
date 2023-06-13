885eecdcaf45481b30ac2abcc796565906ff4f93
Sharon2408 <sharonantoy2408@gmail.com>
2023-06-13 17:39:42 +0530

    timeline

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 const routes: Routes = [
   {
@@ -9,7 +11,14 @@ const routes: Routes = [
   },
   {
     path:"navbar",component:NavbarComponent
+  },
+  {
+    path:"accordion",component:AccordionComponent
+  },
+  {
+    path:"timeline",component:TimelineComponent
   }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,35 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AccordionComponent } from './accordion/accordion.component';
+import { AccordionModule } from 'primeng/accordion';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TimelineModule } from 'primeng/timeline';
+import { TimelineComponent } from './timeline/timeline.component';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
+    AccordionComponent,
+    TimelineComponent,
+    
     
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    BrowserAnimationsModule,
+    TimelineModule,
+    ButtonModule,
+    CardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
